c36930e8e1e649d550454f16be0db7969f14e004
eiji0111 <ee111fender@gmail.com>
2021-06-04 07:37:56 +0000

    [Add]contact_form add

--- app/controllers/public/contacts_controller.rb
+++ app/controllers/public/contacts_controller.rb
@@ -1,11 +1,38 @@
 class Public::ContactsController < ApplicationController
+  before_action :set_contact, only: [:confirm, :back, :create]
   
   def new
+    @contact = Contact.new
   end
 
   def confirm
+    if @contact.invalid?
+      render :new
+    end
+  end
+  
+  def back
+    render :new
+  end
+  
+  def create
+    if @contact.save
+      redirect_to complete_contact_path
+    else
+      render :new
+    end
   end
 
   def complete
+  end
+  
+  private
+  
+  def set_contact
+    @contact = Contact.new(contact_params)
+  end
+  
+  def contact_params
+    params.require(:contact).permit(:name, :email, :content)
   end
 end
