cc1f97860ee5c57218a839c01c6e249ace972695
eiji0111 <ee111fender@gmail.com>
2021-06-15 06:21:50 +0000

    [Add]footer_notification_icon add

--- app/controllers/public/notifications_controller.rb
+++ app/controllers/public/notifications_controller.rb
@@ -2,10 +2,9 @@ class Public::NotificationsController < ApplicationController
   
   def index
     notifications_all = current_customer.passive_notifications
-    notifications_all.where(checked: false).each do |notification| # .update_all(checked: true)
+    notifications_all.where(checked: false).each do |notification|
       notification.update_attributes(checked: true)
     end
-    @notifications = notifications_all.includes(:visitor, :visited)
-                                      .where.not(visitor_id: current_customer.id).page(params[:page])
+    @notifications = notifications_all.includes(:visitor, :visited).where.not(visitor_id: current_customer.id).page(params[:page])
   end
 end
