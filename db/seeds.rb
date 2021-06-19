eb9d72cbae0c109752c47eed0c9f1438318fcc9d
eiji0111 <ee111fender@gmail.com>
2021-06-19 15:49:20 +0000

    [Update]seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,13 +3,31 @@ Admin.create!(
   password: "aaaaaa"
   )
   
-100.times do |n|
+50.times do |n|
   Customer.create!(
     name: Faker::Name.name,
     nickname: Faker::Name.name,
     email: "test#{n + 1}@gmail.com",
     birthday: Date.new(1998, 12, 24),
     sex: 0,
+    password: "aaaaaa",
+    address: 13,
+    one_thing: "よろしくお願いします",
+    introduction: "テストユーザー",
+    hobby: "映画鑑賞",
+    jobs: "テスト",
+    annual_income: 7,
+    current_sign_in_at: Time.zone.now,
+  )
+end
+
+50.times do |n|
+  Customer.create!(
+    name: Faker::Name.name,
+    nickname: Faker::Name.name,
+    email: "test#{n + 1}@gmail.com",
+    birthday: Date.new(1998, 12, 24),
+    sex: 1,
     password: "aaaaaa",
     address: 13,
     one_thing: "よろしくお願いします",
